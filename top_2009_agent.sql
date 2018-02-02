e4a5c1a85c649e4ef5f494e995fc5592456c8556
leah duvic <leaheduvic@gmail.com>
2018-02-02 15:39:54 -0600

    update

--- top_2009_agent.sql
+++ top_2009_agent.sql
@@ -1,8 +1,8 @@
-select name as "Top in Sales", max("Total") as "Total Sales for 2009"
+select name as "Top Salesperson ", max("Total") as "Total"
 from (
-select Employee.FirstName || " " || Employee.LastName as "Name", Employee.Title, sum(Invoice.Total) as "Total"
-from Employee
-join Customer on Customer.SupportRepId = Employee.EmployeeId
-join Invoice on Customer.CustomerId = Invoice.CustomerId
-where Invoice.InvoiceDate between date("2009-01-01") and date("2009-12-31")
+	select Employee.FirstName || " " || Employee.LastName as "Name", Employee.Title, sum(Invoice.Total) as "Total"
+		from Employee
+		join Customer on Employee.EmployeeId = Customer.SupportRepId
+		join Invoice on Customer.CustomerId = Invoice.CustomerId
+		where Invoice.InvoiceDate between "2009-01-01" and "2009-12-31"
 group by Name);
